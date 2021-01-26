dcfe9c1803537e1aefceb2bbff3a4f8af2cf14b0
Tilek M <vvv.tilek@gmail.com>
2021-01-26 15:09:57 +0600

    Refactored for Object RPN

---
+++ CalculatorJAVA/app/src/main/java/com/example/calculatorjava/Ideone.java
@@ -0,0 +1,57 @@
+package com.example.calculatorjava;
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+class Ideone {
+    public static String calc(List<String> postfix) {
+        Deque<Double> stack = new ArrayDeque<Double>();
+        for (String x : postfix) {
+            if (x.equals("sqrt")) stack.push(Math.sqrt(stack.pop()));
+            else if (x.equals("cube")) {
+                Double tmp = stack.pop();
+                stack.push(tmp * tmp * tmp);
+            }
+            else if (x.equals("pow10")) stack.push(Math.pow(10, stack.pop()));
+            else if (x.equals("+")) stack.push(stack.pop() + stack.pop());
+            else if (x.equals("-")) {
+                Double b = stack.pop(), a = stack.pop();
+                stack.push(a - b);
+            }
+            else if (x.equals("*")) stack.push(stack.pop() * stack.pop());
+            else if (x.equals("/")) {
+                Double b = stack.pop(), a = stack.pop();
+                stack.push(a / b);
+            }
+            else if (x.equals("u-")) stack.push(-stack.pop());
+            else stack.push(Double.valueOf(x));
+        }
+
+        return isInteger(""+stack.pop());
+    }
+
+    private static String isInteger(String str_res){
+        char dot = str_res.charAt(str_res.length() - 2);
+        char nul = str_res.charAt(str_res.length() - 1);
+
+        if (dot == '.' && nul == '0'){
+            return str_res.substring(0, str_res.length() - 2);
+        }
+        return str_res;
+    }
+
+//    public static void main (String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        String s = in.nextLine();
+//        ExpressionParser n = new ExpressionParser();
+//        List<String> expression = n.parse(s);
+//        boolean flag = n.flag;
+//        if (flag) {
+//            for (String x : expression) System.out.print(x + " ");
+//            System.out.println();
+//            System.out.println(calc(expression));
+//        }
+//    }
+}
---
+++ CalculatorJAVA/app/src/main/java/com/example/calculatorjava/Model.java
@@ -0,0 +1,139 @@
+package com.example.calculatorjava;
+
+
+import java.util.List;
+
+public class Model {
+    private MainActivity viewer;
+    private Boolean action;
+    private String temp;
+    private String expression;
+    private String log;
+
+    private String numbers = "0123456789";
+    private String operators = "+-*/";
+
+    public Model(MainActivity viewer) {
+        this.viewer = viewer;
+        expression = "";
+        temp = "";
+        log  = "";
+        action = false;
+    }
+
+    public void doAction(String value) {
+
+        if (numbers.contains(value)) {
+            if (expression.length() > 0 && !operators.contains(expression.substring(expression.length() - 1))) {
+                expression = "";
+            }
+
+            if (value.equals("0")) {
+                if (temp.equals("") || temp.equals("0")){
+                    temp = "0";
+                } else {
+                    temp = temp + value;
+                }
+            } else {
+                if (temp.equals("0")){
+                    temp = value;
+                } else {
+                    temp = temp + value;
+                }
+            }
+            action = true;
+        } else if (operators.contains(value)) {
+            if (action){
+                if (expression.equals("")){
+                    expression = temp+" "+value;
+                } else {
+                    expression = expression+" "+temp+" "+value;
+                }
+
+                System.out.println(expression);
+                temp = "";
+                action = false;
+            } else {
+                String operator = expression.substring(expression.length() - 1);
+                if (operators.contains(operator)){
+                    expression = expression.substring(0, expression.length() - 1) + value;
+                }
+            }
+        } else if (value.equals("=")) {
+            String fullExpression = expression + " " + temp;
+            if (expression.length() > 0 && temp.length() > 0) {
+                ExpressionParser n = new ExpressionParser();
+                List<String> parsedExpression = n.parse(fullExpression);
+                temp = parsedExpression.toString();
+
+                if (n.flag) {
+                    log = log + ((log.length() > 0) ? "\n\n\r" : "") + fullExpression + " = ";
+                    for (String x : parsedExpression) System.out.print(x + " ");
+                    temp = Ideone.calc(parsedExpression);
+
+                    if (temp.equals("Infinity") || temp.equals("NaN")) {
+                        temp = "0";
+                        log = log + " Нельзя делить на 0";
+                    } else {
+                        log = log + " " + temp;
+                        expression = temp;
+                        temp = "";
+                    }
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+
+        } else if (value.equals("C")) {
+            log = log + "\n\r";
+            temp = "0";
+            expression = "";
+        } else if (value.equals("AC")){
+            log  = "";
+            temp = "0";
+            expression = "";
+        } else if (value.equals("Del")) {
+            if (temp.length() > 0) {
+                if (temp.length() == 1 || (temp.length() == 2 && temp.charAt(0) == '-')) {
+                    action = false;
+                    temp = "";
+                } else {
+                    temp = temp.substring(0, temp.length() - 1);
+                }
+            } else if (expression.length() > 0) {
+                if (operators.contains(expression.substring(expression.length() - 1))) {
+                    expression = expression.substring(0, expression.lastIndexOf(" "));
+                    if (expression.contains(" ")) {
+                        temp = expression.substring(expression.lastIndexOf(" ") + 1);
+                        expression = expression.substring(0, expression.lastIndexOf(" "));
+                        action = true;
+                    } else {
+                        temp = expression;
+                        expression = "";
+                    }
+                }
+            }
+        } else if (value.equals(",")) {
+            if (!temp.contains(".")) {
+                action = true;
+                if (temp.length() < 1) {
+                    temp = "0.";
+                } else {
+                    temp = temp + ".";
+                }
+            }
+        } else if (value.equals("+/-")) {
+            if (temp.charAt(0) == '-') {
+                temp = temp.substring(1);
+            } else {
+                temp = '-' + temp;
+            }
+        }
+
+        viewer.setFontSize(new String(expression + " " + temp).length());
+        viewer.update(expression + " " + temp, log);
+    }
+
+}
